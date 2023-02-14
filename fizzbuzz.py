ca24740a6323bad705568d914bcabc0d8694d80d
tolossamuel <98914406+tolossamuel@users.noreply.github.com>
2023-02-14 15:56:47 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,18 @@
 class Solution:
-    m=[]
-    b=1
     def fizzBuzz(self, n: int) -> List[str]:
-        while(Solution.b<=n):
-            if((Solution.b)%3==0 and (Solution.b)%5==0):
-                Solution.m.append("FizzBuzz")
-            elif((Solution.b)%3==0 and (Solution.b)%5!=0):
-                Solution.m.append("Fizz")
-            elif((Solution.b)%5==0 and (Solution.b)%3!=0):
-                Solution.m.append("Buzz")
+        m=[]
+        b=1
+        while(b<=n):
+            if((b)%15==0):
+                m.append("FizzBuzz")
+            elif((b)%3==0):
+                m.append("Fizz")
+            elif((b)%5==0):
+                m.append("Buzz")
             else:
-                Solution.b=str(Solution.b)
-                Solution.m.append(Solution.b)
-            Solution.b=int(Solution.b)
-            Solution.b+=1
-        if(len(Solution.m)>n):
-            Solution.b=len(Solution.m)-n
-            for i in range(Solution.b):
-                Solution.m.pop()
+                b=str(b)
+                m.append(b)
+            b=int(b)
+            b+=1
+        return m
         return Solution.m
