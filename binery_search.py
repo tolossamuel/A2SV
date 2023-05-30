c2404bdf1e0f478b768c018f8dbe2cf0ada4623a
Samuel Tolossa <98914406+tolossamuel@users.noreply.github.com>
2023-05-30 21:02:25 +0300

    Create binery_search.py

---
+++ binery_search.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    c=0
+    for i in range(len(a)):
+        for y in range(len(a)-1):
+            if(a[y]>a[y+1]):
+                a[y],a[y+1]=a[y+1],a[y]
+                c+=1
+    print(f"Array is sorted in {c} swaps.")
+    print(f"First Element: {a[0]}\nLast Element: {a[len(a)-1]}")
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
